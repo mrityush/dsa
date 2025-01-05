1523d47ca25a47d4673063147c553f7aeec2cc72
mj <mrityunjaya.shukla@gmail.com>
2025-01-05 13:18:19 +0530

    ut framework added

--- common/LogExecutionTime.ts
+++ common/LogExecutionTime.ts
@@ -1,21 +1,15 @@
-// common/LogExecutionTime.ts
-export function LogExecutionTime(target: any, propertyKey: any, descriptor?: PropertyDescriptor): void {
+export function LogExecutionTime(target: any, propertyKey: string, descriptor?: PropertyDescriptor): void {
   if (!descriptor) {
-    descriptor = {
-      value: target[propertyKey]
-    };
+    throw new Error('Descriptor is required');
   }
 
   const originalMethod = descriptor.value;
 
   descriptor.value = function (...args: any[]) {
     const start = performance.now();
-    const result = target.apply(this, args);
+    const result = originalMethod.apply(this, args);
     const end = performance.now();
     console.log(`${propertyKey} executed in ${end - start}ms`);
     return result;
   };
-
-  descriptor.value();
-
 }
